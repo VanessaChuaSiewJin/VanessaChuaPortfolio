d9323108024c018299f3616985f85b7eae9d69ac
VanessaChuaSiewJin <vanessa.chuasiewjin.2000@gmail.com>
2026-02-15 03:41:16 +0800

    Update Resume (3)

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -45,8 +45,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="resume.pdf"
+                    href={require("./Vanessa_Chua_Resume.pdf")}
+                    download="Vanessa_Chua_Resume.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
